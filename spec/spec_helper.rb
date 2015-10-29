b6335c3ad3847409c9516fbad661afe3f2602c4a
oakeem <akeem.oduola@yahoo.com>
2015-10-29 06:42:03 +0100

    edited gemspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'coveralls'
 Coveralls.wear!
-
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 require 'pry'
 require 'zola'
